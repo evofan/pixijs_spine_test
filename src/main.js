fdb801a6d91fdb2e84b9a781fbdb8050192353a0
evofan <evofan@outlook.jp>
2019-07-16 22:37:22 +0900

    tweak

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ const ANIMATION = "animation";
 
 // init
 let app = new PIXI.Application({
-  width: WIDTH,
-  height: HEIGHT
+	width: WIDTH,
+	height: HEIGHT
 });
 let canvas = document.getElementById("canvas");
 canvas.appendChild(app.view);
@@ -17,35 +17,33 @@ app.stage.interactive = true;
 let bg, spine;
 
 PIXI.loader
-  .add("spine_data", ASSET_SPINE)
-  .add("bg_data", ASSET_BG)
-  .load(onAssetsLoaded)
+	.add("spine_data", ASSET_SPINE)
+	.add("bg_data", ASSET_BG)
+	.load(onAssetsLoaded);
 
 /**
  * Asset load Complete
  * @param { object } loader object
- * @param { object} res asset data
+ * @param { object } res asset data
  */
 function onAssetsLoaded(loader, res) {
+	bg = new PIXI.Sprite(res.bg_data.texture);
+	app.stage.addChild(bg);
+	bg.x = 0;
+	bg.y = 0;
 
-  bg = new PIXI.Sprite(res.bg_data.texture);
-  app.stage.addChild(bg);
-  bg.x = 0;
-  bg.y = 0;
+	spine = new PIXI.spine.Spine(res.spine_data.spineData);
+	spine.scale.set(0.5);
+	spine.x = 100;
+	spine.y = 30;
 
-  spine = new PIXI.spine.Spine(res.spine_data.spineData);
-  spine.scale.set(0.5);
-  spine.x = 100;
-  spine.y = 30;
-
-  spine.state.setAnimation(0, ANIMATION, false);
-  app.stage.addChild(spine);
-
+	spine.state.setAnimation(0, ANIMATION, false);
+	app.stage.addChild(spine);
 }
 
 /**
  * start spine animation
  */
 function startAnimation() {
-  spine.state.setAnimation(0, ANIMATION, false);
-};
+	spine.state.setAnimation(0, ANIMATION, false);
+}
